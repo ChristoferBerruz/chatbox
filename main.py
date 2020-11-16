f47d9ae2365a5f7d37ffa9677fb03e9a066c0a0d
Christofer Berruz <cberruzc@my.bridgeport.edu>
2020-11-15 22:45:36 -0500

    Asking for profession

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     username = input("What is your name? ")
     greeting = "Welcome {}!".format(username)
     print(greeting)
+    profession = input("What is your profession? ")
 
 if __name__ == '__main__':
     main()
